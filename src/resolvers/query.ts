9ee9eaf574eb1326f86e2edd1c4c27e1cf2a27cc
Wilkel Giovanni <wilkelgiovanni@gmail.com>
2020-05-07 16:24:00 -0400

    Definición del schema con la primera definición y tipo de objeto season

--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -2,15 +2,7 @@ import { IResolvers } from 'graphql-tools';
 
 const query: IResolvers = {
     Query: {
-        hello(): string {
-            return 'Hello world!!';
-        },
-        helloWithName(_: void, args): string {
-            return `Hello ${args.name}!!`;
-        },
-        helloToGraphQLCourse(): string {
-            return 'Hello to GraphQL Course!!';
-        }
+        
     }
 };
 
