0ef2536c239ab5c59ab1cde3344fa90f69951342
Wilkel Giovanni <wilkelgiovanni@gmail.com>
2020-05-07 16:32:20 -0400

    Lista de temporadas de toda la historia de la Formula 1

--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -2,6 +2,11 @@ import { IResolvers } from 'graphql-tools';
 
 const query: IResolvers = {
     Query: {
+        async seasonsList(_:void, __:any, { dataSources }) {
+            return await dataSources.seasons.getSeasons().then(
+                (data: any) => data.MRData.SeasonTable.Seasons
+            );
+        } 
         
     }
 };
